2a7386972c4b4666ccda83a3bbd60f007f62eb35
Jiseeeh <johncarlo.camara1@gmail.com>
2023-02-23 01:03:22 +0800

    add: add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# yay
+sudo pacman -S yay
+
+# things i need for web dev
+sudo pacman -S nodejs npm neovim && yay -S visual-studio-code-bin google-chrome 
+sudo npm i -g yarn pnpm
+
+echo "Do you want to install other apps?"
+echo "If yes, input y otherwise you can input anything."
+
+read answer
+
+if [ "$answer" = "y" ]
+then
+    # others
+    sudo pacman -S gnome-boxes && yay -S logseq-desktop-bin 
+else 
+    echo "Done!"
+fi
